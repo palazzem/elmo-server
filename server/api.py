4d95b4cf0707a520544aa2df31d2e83b0c2156f4
Emanuele Palazzetti <hello@palazzetti.me>
2019-05-20 16:32:09 +0200

    [core/api] add Application settings

--- server/api.py
+++ server/api.py
@@ -1,7 +1,11 @@
 import falcon
 
+from .conf import settings
 from .resources import AreasResource
 
+
+# Check required configuration
+settings.is_valid()
 
 # Initialize the API
 api = falcon.API()
