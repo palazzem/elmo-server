bb08f40a394fff1d35c5b7cabbd3c2b54ec5830d
Emanuele Palazzetti <hello@palazzetti.me>
2019-05-20 20:59:48 +0200

    [core/api] implement authentication endpoint

--- server/api.py
+++ server/api.py
@@ -1,28 +1,16 @@
 import falcon
-import logging
 
-from elmo.settings.exceptions import ConfigNotValid
-
-from .conf import settings
-from .resources import Authentication, AreasResource
-
-
-log = logging.getLogger(__name__)
+from .resources import Authentication
 
 
 def create():
-    """Create a Falcon application to expose a REST API for the Elmo System.
+    """Create a Falcon application to expose an API for the Elmo System.
     A valid configuration is required otherwise `None` is returned.
-    """
-    try:
-        settings.is_valid()
-    except ConfigNotValid as e:
-        # The application cannot start without a valid configuration
-        log.critical(e)
-        return None
 
+    Returns:
+        A `falcon.API()` instance, populated with API endpoints for Elmo System
+    """
     # Initialize the API
     api = falcon.API()
     api.add_route("/api/v0/auth", Authentication())
-    api.add_route("/api/v0/areas", AreasResource())
     return api
--- server/resources.py
+++ server/resources.py
@@ -1,16 +1,54 @@
-class AreasResource(object):
-    """Areas resource to arm/disarm all the alerts for an area.
-    It requires an authenticated access token.
+import falcon
+import logging
 
-    Usage:
-        PUT /api/v1/areas/: arm the status of all alerts
-        DELETE /api/v1/areas/: disarm the status of all alerts
+from elmo.api.client import ElmoClient
+from elmo.api.exceptions import APIException, PermissionDenied
+
+from .conf import settings
+
+
+log = logging.getLogger(__name__)
+
+
+class Authentication(object):
+    """Authentication endpoint to retrieve an access token.
+
+    Endpoints:
+        POST /api/v1/auth/
     """
 
-    def on_put(self, req, resp):
-        """TODO"""
-        pass
+    def on_post(self, req, resp):
+        """Authenticate the user and returns an access token.
 
-    def on_delete(self, req, resp):
-        """TODO"""
-        pass
+        Expected request:
+        {
+            "username": "str",
+            "password": "str
+        }
+
+        Args:
+            req: Falcon `Request` object
+            resp: Falcon `Response` object
+        Raises:
+            HTTPBadRequest: if the request misses mandatory fields
+            HTTPForbidden: if credentials are not valid
+            HTTPServiceUnavailable: if the Elmo System returns a server error
+        Returns:
+            A JSON response with the access token.
+        """
+        client = ElmoClient(settings.base_url, settings.vendor)
+        username = req.media.get("username")
+        password = req.media.get("password")
+
+        if username is None or password is None:
+            raise falcon.HTTPBadRequest(description="Missing username and password")
+        try:
+            token = client.auth(username, password)
+        except PermissionDenied:
+            raise falcon.HTTPForbidden(description="Wrong username or password")
+        except APIException as e:
+            log.error("503 ServiceUnavailable: {}".format(e))
+            raise falcon.HTTPServiceUnavailable(description="".format(e))
+
+        resp.media = {"access_token": token}
+        resp.status = falcon.HTTP_200
