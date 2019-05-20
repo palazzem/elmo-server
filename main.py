6cda98dac468a89ea3bc17df53c7e08bbae47477
Emanuele Palazzetti <hello@palazzetti.me>
2019-05-20 20:58:51 +0200

    [core/api] add Settings validity check when server starts

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-from server.api import create
+import logging
 
+from server.api import create
+from server.conf import settings
+from elmo.settings.exceptions import ConfigNotValid
+
+
+log = logging.getLogger(__name__)
+
+try:
+    # The application must not start without a valid configuration
+    settings.is_valid()
+except ConfigNotValid as e:
+    log.critical(e)
+    exit(1)
 
 # Use AppEngine default entrypoint (gunicorn)
 app = create()
