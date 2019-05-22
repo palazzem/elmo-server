e0980963828a75f345dd6e5972879edd171bc39c
Emanuele Palazzetti <hello@palazzetti.me>
2019-05-22 18:25:30 +0200

    [core/api] implement DISARM endpoint

--- server/resources.py
+++ server/resources.py
@@ -83,7 +83,7 @@ class AlarmsResource(object):
             HTTPBadRequest: if the request misses mandatory fields
             HTTPServiceUnavailable: if the Elmo System returns a server error
         Returns:
-            A JSON response with the access token.
+            A JSON response with the alarms status.
         """
         code = req.media.get("code")
         if code is None:
@@ -111,3 +111,51 @@ class AlarmsResource(object):
 
         resp.status = falcon.HTTP_200
         resp.media = {"alarms_armed": True}
+
+    def on_delete(self, req, resp, token):
+        """Disarm all alarms after gaining the system lock. Once the operation is
+        completed with success, the system lock is released.
+        The endpoint requires an `Authorization` header with a valid bearer
+        token.
+
+        Expected request:
+        {
+            "code": "str"
+        }
+
+        Args:
+            req: Falcon `Request` object
+            resp: Falcon `Response` object
+            token: Bearer token that must be used to make the client request
+        Raises:
+            HTTPBadRequest: if the request misses mandatory fields
+            HTTPServiceUnavailable: if the Elmo System returns a server error
+        Returns:
+            A JSON response with the alarms status.
+        """
+        code = req.media.get("code")
+        if code is None:
+            raise falcon.HTTPBadRequest(description="`code` is a required field")
+
+        # Initialize the client with a bearer token
+        client = ElmoClient(settings.base_url, settings.vendor)
+        client._session_id = token
+
+        try:
+            with client.lock(code):
+                client.disarm()
+        except PermissionDenied:
+            raise falcon.HTTPUnauthorized(
+                description="The bearer token is invalid or expired"
+            )
+        except APIException as e:
+            # This status may lead to a case where the system is locked.
+            # The global system lock is automatically released after one
+            # minute. Unfortunately it's not possible to recover this state
+            # other than providing credentials in every call (making useless
+            # the bearer token).
+            log.error("503 ServiceUnavailable: {}".format(e))
+            raise falcon.HTTPServiceUnavailable(description="".format(e))
+
+        resp.status = falcon.HTTP_200
+        resp.media = {"alarms_armed": False}
